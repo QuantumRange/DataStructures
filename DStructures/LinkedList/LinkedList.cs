05f351b821b82dbc3e753ce67d59c225a0eab10f
QuantumRange <quantumrange@mailbox.org>
2021-10-25 11:55:09 +0200

    Add Method

--- DStructures/LinkedList/LinkedList.cs
+++ DStructures/LinkedList/LinkedList.cs
@@ -18,9 +18,13 @@ namespace DStructures.LinkedList
         /// <summary>
         /// Stores the Last Element of the LinkedList.
         /// </summary>
-        public ILinkedListItem Last { get; set; }
-        public int Count { get; }
+        private ILinkedListItem Last { get; set; }
         
+        /// <summary>
+        /// Stores the Size of the LinkedList for Performance reasons.
+        /// </summary>
+        public int Count { get; private set; }
+
         /// <summary>
         /// Initialize the values in the constructor.
         /// </summary>
@@ -32,10 +36,31 @@ namespace DStructures.LinkedList
         /// <summary>
         /// Add values to the LinkedList.
         /// </summary>
-        /// <param name="item">is the value </param>
+        /// <param name="item">is one element of the list.</param>
         public void Add(ILinkedListItem item)
         {
-            
+            // Adding one to the size of the LinkedList
+            Count++;
+
+            /*
+             * If no items exist in the LinkedList set the First and Last Variables to the new item.
+             * Otherwise set the Last variable to the item to append it.
+             */
+            if (First == null || Last == null)
+            {
+                First = item;
+                Last = item;
+            }
+            else
+            {
+                // Stores the referens of the "old" last item.
+                ILinkedListItem i = Last;
+
+                // Set the new last item.
+                Last = item;
+                // Set the reference for the last item.
+                i.Next = Last;
+            }
         }
 
         public void RemoveAt(int i)
@@ -45,7 +70,7 @@ namespace DStructures.LinkedList
 
         public void RemoveLast()
         {
-            throw new NotImplementedException();
+            
         }
 
         public ILinkedListItem GetAt(int i)
